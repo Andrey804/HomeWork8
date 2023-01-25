207bb697bf87abf01c971a005b200ba7bd6032e1
AndreyBoiko <75693099+AndreyBoiko@users.noreply.github.com>
2023-01-25 17:54:48 +0200

    Now the program works with date of type "str"

--- main.py
+++ main.py
@@ -2,19 +2,33 @@ from datetime import datetime, timedelta
 from collections import defaultdict
 import re
 
+# users = [
+#     {"name": "Bill",
+#      "birthday": datetime(year=1999, month=1, day=26)},
+#     {"name": "Jill",
+#      "birthday": datetime(year=2023, month=1, day=21)},
+#     {"name": "Kim",
+#      "birthday": datetime(year=2000, month=1, day=29)},
+#     {"name": "Jan",
+#      "birthday": datetime(year=1991, month=4, day=23)},
+#     {"name": "Mary",
+#      "birthday": datetime(year=2002, month=1, day=25)},
+#     {"name": "Anna",
+#      "birthday": datetime(year=1998, month=1, day=28)}
+# ]
 users = [
     {"name": "Bill",
-     "birthday": datetime(year=1999, month=1, day=26)},
+     "birthday": "26-01-1999"},
     {"name": "Jill",
-     "birthday": datetime(year=2023, month=1, day=21)},
+     "birthday": "21-01-2023"},
     {"name": "Kim",
-     "birthday": datetime(year=2000, month=1, day=29)},
+     "birthday": "29-01-2000"},
     {"name": "Jan",
-     "birthday": datetime(year=1991, month=4, day=23)},
+     "birthday": "23-04-1991"},
     {"name": "Mary",
-     "birthday": datetime(year=2002, month=1, day=25)},
+     "birthday": "26-01-2002"},
     {"name": "Anna",
-     "birthday": datetime(year=1998, month=1, day=28)}
+     "birthday": "25-01-1998"}
 ]
 
 
@@ -30,21 +44,29 @@ def get_birthdays_per_week(users_list):
             if k == "name":
                 name_user = v
                 continue
+            user_with_current_date = None
 
-            user_with_current_year = v.replace(year=start_interval.year).date()
+            if type(v) == datetime:
+                user_with_current_date = v.replace(year=start_interval.year).date()
+            elif type(v) == str and re.search(r"^\d\d.\d\d.\d\d\d\d$", v):
+                user_with_current_date = datetime(day=int(v[:2]), month=int(v[3:5]), year=start_interval.year).date()
+            else:
+                print(f"The user '{name_user}' has an invalid date type.")
+                continue
 
-            if start_interval <= user_with_current_year <= end_interval:
+            if start_interval <= user_with_current_date <= end_interval:
                 if start_interval.weekday() != 0:
-                    if user_with_current_year.weekday() == 5 or user_with_current_year.weekday() == 6:
+                    if user_with_current_date.weekday() == 5 or user_with_current_date.weekday() == 6:
                         by_days["Monday"].append(name_user)
                     else:
-                        by_days[user_with_current_year.strftime('%A')].append(name_user)
+                        by_days[user_with_current_date.strftime('%A')].append(name_user)
                 else:
-                    if user_with_current_year.weekday() != 5 and user_with_current_year.weekday() != 6:
-                        by_days[user_with_current_year.strftime('%A')].append(name_user)
+                    if user_with_current_date.weekday() != 5 and user_with_current_date.weekday() != 6:
+                        by_days[user_with_current_date.strftime('%A')].append(name_user)
 
     for k, v in by_days.items():
         a = r'[\[\]\']'
         print(f"{k}: {re.sub(a, '', str(v))}")
+
 
 get_birthdays_per_week(users)
